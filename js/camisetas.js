b39032238c38fad50b13e65b61c10904da06e5ef
Michel <m1tchofc@gmail.com>
2022-11-15 16:07:54 +0000

    corrigido 3

--- js/camisetas.js
+++ js/camisetas.js
@@ -1,44 +1,44 @@
 const database = [
-    {
-      id: 1,
-      img: "img/jaqueta.svg",
-      nameItem: "Lightweight Jacket",
-      description:
-        "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
-      value: 100,
-      addCart: "Adicionar ao carrinho",
-      tag: ["Camisetas"],
-    },
-    {
-      id: 4,
-      img: "img/camiseta_preta.svg",
-      nameItem: "T-Shirt",
-      description:
-        "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
-      value: 100,
-      addCart: "Adicionar ao carrinho",
-      tag: ["Camisetas"],
-    },
-    {
-      id: 5,
-      img: "img/camiseta_branca.svg",
-      nameItem: "Short-Sleeve T-Shirt",
-      description:
-        "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
-      value: 100,
-      addCart: "Adicionar ao carrinho",
-      tag: ["Camisetas"],
-    },
-    {
-      id: 6,
-      img: "img/moletom.svg",
-      nameItem: "Champion Packable Jacket",
-      description:
-        "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
-      value: 100,
-      addCart: "Adicionar ao carrinho",
-      tag: ["Camisetas"],
-    },
+  {
+    id: 1,
+    img: "img/jaqueta.svg",
+    nameItem: "Lightweight Jacket",
+    description:
+      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 4,
+    img: "img/camiseta_preta.svg",
+    nameItem: "T-Shirt",
+    description:
+      "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 5,
+    img: "img/camiseta_branca.svg",
+    nameItem: "Short-Sleeve T-Shirt",
+    description:
+      "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 6,
+    img: "img/moletom.svg",
+    nameItem: "Champion Packable Jacket",
+    description:
+      "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
 ]
 let contador = 0 
 let preco = 0
